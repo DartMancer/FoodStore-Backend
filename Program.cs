fe8f91266aad46566a116a820205d90865a2cfa1
vkurseApp <vkurseapp@yandex.ru>
2024-05-01 23:21:56 +0300

    Some Changes
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Добавление CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyAllowSpecificOrigins",
+        policyBuilder =>
+        {
+            policyBuilder.WithOrigins("http://localhost:5148")
+                         .AllowAnyHeader()
+                         .AllowAnyMethod()
+                         .AllowCredentials();
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +30,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Применение CORS политики
+app.UseCors("MyAllowSpecificOrigins");
 
 app.UseAuthorization();
 
